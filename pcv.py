d462530ed3fd90ac928f38c61a2e5b0789fd5cd1
Gabriel Leite <42241006+gabrielleite751@users.noreply.github.com>
2021-11-29 07:55:50 -0300

    Update pcv.py

--- pcv.py
+++ pcv.py
@@ -1,9 +1,20 @@
 import sys
 import time
 
-citys = 4
+
+citys = 15 # Number of cities
+
 
 def tsp(matrix, init):
+	""" Return the shortest way
+        
+        Args:
+        	matrix (array): Matrix with the distance between cities.
+			init (int): City of origin.
+        
+    	Returns:
+        	list: Fastest way list.
+    """
 	min_path = sys.maxsize
 	path_list = []
 	aux = []
@@ -14,7 +25,6 @@ def tsp(matrix, init):
 			aux.append(i)
 
 	while True:
-		print(path)
 		current_path_weight = 0
 		
 		k = init
@@ -26,14 +36,23 @@ def tsp(matrix, init):
 
 		if current_path_weight < min_path:
 			min_path = current_path_weight
-			path = aux
+			path = aux.copy()
 
-		if not next_perm(aux):
+		if not next_permutation(aux):
 			break
 
 	return path
 
-def next_perm(aux):
+
+def next_permutation(aux):
+    """ Check if there is a next permutation.
+        
+        Args:
+        	aux (list): list with actual permutation.
+        
+        Returns:
+        	bool: Whether if the operation was sucessful or not.
+    """
     n = len(aux)
     i = n-2
 
@@ -59,44 +78,48 @@ def next_perm(aux):
         right -= 1
     return True
 
-def output_print(path_list,path, matrix, init):
-	print('*------- AV2 -------*')
-	print('Equipe:\n\tGabriel Leite;\n\tRenan Didier;\n\tJoão Dowsley')
-	print('Comentário:\n\tSolução desenvolvida com Programação Dinâmica')
-	print(f'Numero total de cidades: {citys}')
-	print(f'Numero de cidades da rota: {len(path)+1}')
-	print(f'Sequencia da rota:\n\t', end='')
+
+def output_print(path, matrix, init):
+	print('---------------------------------------------------------------')
+	print('|                             AV2                             |')
+	print('---------------------------------------------------------------')
+	print('\nEquipe:\n\tGabriel Leite;\n\tRenan Didier;\n\tJoão Dowsley')
+	print('\nComentário:\n\tSolução desenvolvida ultilizando método de Força Bruta')
+	print(f'\nNumero total de cidades: {citys}')
+	print(f'\nNumero de cidades da rota: {len(path)+1}')
+	print(f'\nSequencia da rota:\n\t', end='')
 	print(f'{init}', end=' -> ')
 	for i in path:
 		print(f'{i}', end=' -> ')
 	print(init)
-	print(f"--- {(time.time() - start_time)} seconds ---")
+	print(f"\nTempo de execução: {(time.time() - start_time)} seconds")
+
 
 if __name__ == "__main__":
 	# Matriz exemplo
 	
 	start_time = time.time()
 
-	#matrix = [[ 0,  5, 15, 20,  3,  8, 10,  2, 14, 20,  9, 17, 11,  6, 10], #1
-    #          [ 5,  0, 11,  1,  4,  2, 14, 12,  3,  4,  6, 18, 14, 19,  3], #2
-    #          [15, 11,  0, 16, 15,  3,  5,  7, 14, 18, 17,  3, 13,  9, 10], #3
-    #          [20,  1, 16,  0, 14,  4, 13,  4, 16, 19, 20, 13,  9, 12, 11], #4
-    #          [ 3,  4, 15, 14,  0, 15, 18, 14,  3,  5, 10,  6, 14, 10,  4], #5
-    #          [ 8,  2,  3,  4, 15,  0, 10, 11,  4,  8,  9, 11, 23, 12,  7], #6
-    #          [10, 14,  5, 18, 14, 10,  0, 12, 37, 11, 56,  1,  6,  9, 10], #7
-    #          [ 2, 12,  7,  4, 14, 11, 12,  0, 13, 18, 20, 21, 47,  1,  4], #8
-    #          [14,  3, 14, 16,  3,  4, 37, 12,  0,  9,  7, 10, 20, 45,  3], #9
-    #          [20,  4, 18, 19,  5,  8, 11, 18,  9,  0,  1,  7,  5,  6,  1], #10
-    #          [ 9,  6, 17, 20, 10,  9, 56, 20,  7,  1,  0,  3,  2,  5,  1], #11
-    #          [17, 18,  3, 13,  6, 11,  1, 21, 10,  7,  3,  0,  4,  1,  8], #12
-    #          [11, 14, 13,  9, 14, 23,  6, 47, 20,  5,  2,  4,  0, 3 ,  1], #13
-    #          [ 6, 19,  9, 12, 10, 12,  9,  1,  6, 45,  6,  1, 63,  0, 12], #14
-    #          [10,  3, 10, 11,  4,  7, 10,  4, 19,  3,  1,  8,  4,  1,  0]] #15
+		    #   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15
+	matrix = [[ 0,  5, 15, 20,  3,  8, 10,  2, 14, 20,  9, 13, 11,  6, 12], #1
+              [ 5,  0,  1,  1,  4,  2, 14, 12,  3,  4,  6, 18, 14, 19,  7], #2
+			  [15,  1,  0, 16, 15,  3,  5,  7, 14, 18, 17,  3, 13,  9, 10], #3
+          	  [20,  1, 16,  0, 14,  4, 13,  4, 16, 19, 20, 13,  9, 12, 11], #4
+          	  [ 3,  4, 15, 14,  0, 15, 18, 14,  3,  5, 10,  6, 14, 10,  4], #5
+           	  [ 8,  2,  3,  4, 15,  0, 10, 11,  4,  8,  9, 11, 23, 12,  7], #6
+           	  [10, 14,  5, 18, 14, 10,  0, 12, 37, 11, 56,  1,  6,  9, 10], #7
+              [ 2, 12,  7,  4, 14, 11, 12,  0, 13, 18, 20, 21, 47,  1,  4], #8
+           	  [14,  3, 14, 16,  3,  4, 37, 12,  0,  9,  7, 10, 20, 45,  3], #9
+           	  [20,  4, 18, 19,  5,  8, 11, 18,  9,  0,  1,  7,  5,  6,  1], #10
+           	  [ 9,  6, 17, 20, 10,  9, 56, 20,  7,  1,  0,  3,  2,  5,  1], #11
+           	  [13, 18,  3, 13,  6, 11,  1, 21, 10,  7,  3,  0,  4,  1,  8], #12
+              [11, 14, 13,  9, 14, 23,  6, 47, 20,  5,  2,  4,  0, 3 ,  1], #13
+           	  [ 6, 19,  9, 12, 10, 12,  9,  1,  6, 45,  6,  1, 63,  0, 12], #14
+           	  [12,  7, 10, 11,  4,  7, 10,  4, 19,  3,  1,  8,  4,  1,  0]] #15
 
-	matrix = [[0,10,15,20], [10,0,35,25], [15,35,0,30], [20,25,30,0]]
+	#matrix = [[0,10,15,20], [10,0,35,25], [15,35,0,30], [20,25,30,0]] # Simple test case
+	
+	init = 0 # City of origin
 
-	init = 0
-    
 	path = tsp(matrix, init)
-	print(path)
-	output_print(path_list, path, matrix, init)
+	output_print(path, matrix, init)
